1376bbf656703b43e91449627b6124927e8f9df9
Armin Oonk <Armin@berkelaarmrt.com>
2017-12-10 19:10:02 +0100

    day 6 part 2

--- day06.py
+++ day06.py
@@ -20,6 +20,8 @@ nr_banks = len(memory)
 nr_cycles = 0
 history = []
 history.append(' '.join(map(str, memory)))
+repeat_state = ''
+first = 0
 
 while True:
     nr_cycles += 1
@@ -31,8 +33,16 @@ while True:
         memory[(index + i + 1) % nr_banks] += 1
 
     current_state = ' '.join(map(str, memory))
-    print(str(nr_cycles) + ': ' + current_state)
+    # print(str(nr_cycles) + ': ' + current_state)
 
-    if current_state in history:
+    if current_state in history and not repeat_state:
+        repeat_state = current_state
+        print('First time: ' + str(nr_cycles) + ': ' + current_state)
+        first = nr_cycles
+
+    if repeat_state == current_state and nr_cycles != first:
+        print('Second time: ' + str(nr_cycles) + ': ' + current_state)
+        print('diff: ' + str(nr_cycles-first))
         break
+
     history.append(current_state)
