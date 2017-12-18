73de06c5efdff1a85f0668892e2f7c5875beb229
Armin Oonk <Armin@berkelaarmrt.com>
2017-12-18 21:47:58 +0100

    day 15 part 2

--- day15.py
+++ day15.py
@@ -1,10 +1,13 @@
 class Generator:
-    def __init__(self, factor, first):
+    def __init__(self, factor, first, sanity_check):
         self.factor = factor
         self.current = first
+        self.sanity_check = sanity_check
 
     def run(self):
         self.current = (self.current * self.factor % 2147483647)
+        while (self.current % self.sanity_check) != 0:
+            self.current = (self.current * self.factor % 2147483647)
         return self.current
 
     def compare_16bit(self, other):
@@ -14,21 +17,21 @@ class Generator:
 gen_a_factor = 16807
 gen_b_factor = 48271
 # Example
-# a = Generator(factor=16807, first=65)
-# b = Generator(factor=48271, first=8921)
+# a = Generator(factor=16807, first=65, sanity_check=4)
+# b = Generator(factor=48271, first=8921, sanity_check=8)
 # Input
-a = Generator(factor=16807, first=277)
-b = Generator(factor=48271, first=349)
+a = Generator(factor=16807, first=277, sanity_check=4)
+b = Generator(factor=48271, first=349, sanity_check=8)
 
-# print('a\t\tb')
+print('a\t\tb')
 score = 0
-for _ in range(0, 40000000):
+for _ in range(0, 5000000):
     a.run()
     b.run()
-    # print(str(a.run()) + '\t\t' + str(b.run()))
+    # print(str(a.current) + '\t\t' + str(b.current))
     if a.compare_16bit(b):
         # print("Match! 16bit")
         score += 1
-    if (_ % 1000000) == 0:
+    if (_ % 200000) == 0:
         print(str(_))
 print('Score: ' + str(score))
