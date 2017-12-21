bc4350a6e4c244b96e0a7cb7935daf06d2cc9471
Armin Oonk <Armin@berkelaarmrt.com>
2017-12-21 22:05:19 +0100

    day 17 part 2

--- day17.py
+++ day17.py
@@ -1,17 +1,18 @@
-
-buffer = [0]
+buffer = [0, 1]
 # puzzle_input = 2017
 puzzle_input = 328
-current_pos = 0
+current_pos = 1
 
-for i in range(1, 2018):
-    index = (current_pos + puzzle_input) % len(buffer)
-    buffer.insert(index+1, i)
+for i in range(2, 50000000+1):
+    index = (current_pos + puzzle_input) % (i)  # len(buffer)
+    if index == 0:
+        buffer.insert(index + 1, i)
     current_pos = index + 1
 
-    # print(str(index) + ' ' + str(buffer))
+    # print(str(index) + ' ' + str(buffer) + ' len(buffer): ' + str(len(buffer)))
+    # print(str(i) + ' ' + str(len(buffer)))
 
 index_of_zero = buffer.index(0)
 
-print("Next value: " + str(buffer[current_pos+1]))
-print("Value after 0 value: " + str(buffer[index_of_zero+1]))
+# print("Next value: " + str(buffer[current_pos + 1]))
+print("Value after 0 value: " + str(buffer[index_of_zero + 1]) + ' index of zero: ' + str(index_of_zero))
