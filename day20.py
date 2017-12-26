d2a83bb96c7a3e731404f3f8938dbf38e7ba0e04
Armin Oonk <Armin@berkelaarmrt.com>
2017-12-26 14:49:42 +0100

    day 20 part 2

--- day20.py
+++ day20.py
@@ -1,33 +1,47 @@
-def dist(v):
-    return sum(map(abs, v))
-
-
-def get_position(s, t):
-    ret = []
-    ret.append(0.5 * s['a'][0] * t * t + s['v'][0] * t + s['p'][0])
-    ret.append(0.5 * s['a'][1] * t * t + s['v'][1] * t + s['p'][1])
-    ret.append(0.5 * s['a'][2] * t * t + s['v'][2] * t + s['p'][2])
-    return ret
-
+import math
 
 with open('day20Input.txt', 'r') as f:
     data = f.readlines()
 
+
+class Particle:
+    def __init__(self, d):
+        vals = d.split(' ')
+        self.p = list(map(int, vals[0][vals[0].index('<') + 1: vals[0].index('>')].split(',')))
+        self.v = list(map(int, vals[1][vals[1].index('<') + 1: vals[1].index('>')].split(',')))
+        self.a = list(map(int, vals[2][vals[2].index('<') + 1: vals[2].index('>')].split(',')))
+        self.alive = True
+
+    def integrate(self):
+        for i in range(0, 3):
+            self.v[i] += self.a[i]
+            self.p[i] += self.v[i]
+
+    def distance(self, other):
+        diff = []
+        for i in range(0, 3):
+            diff.append(self.p[i]-other.p[i])
+        return sum(map(abs, diff))
+
+
 state = []
 
 for d in data:
-    ret = dict()
-    vals = d.split(' ')
-    ret['p'] = list(map(int, vals[0][vals[0].index('<') + 1: vals[0].index('>')].split(',')))
-    ret['v'] = list(map(int, vals[1][vals[1].index('<') + 1: vals[1].index('>')].split(',')))
-    ret['a'] = list(map(int, vals[2][vals[2].index('<') + 1: vals[2].index('>')].split(',')))
-    state.append(ret)
+    state.append(Particle(d))
 
-smallest_accel = 99999999999999999999
-for i, s in enumerate(state):
-    p = dist(s['p'])
-    v = dist(s['v'])
-    a = dist(s['a'])
-    if a <= smallest_accel:
-        smallest_accel = a
-        print("Smallest accel: " + str(smallest_accel) + ' at ' + str(i) + ' p far away: ' + str(dist(get_position(s, 10000))))
+cnt = 0
+while True:
+    for i in range(0, len(state)):
+        for j in range(i + 1, len(state)):
+            if state[i].distance(state[j]) == 0:
+                state[i].alive = False
+                state[j].alive = False
+
+    state = [x for x in state if x.alive]
+
+    for s in state:
+        s.integrate()
+
+    cnt += 1
+    if cnt % 200:
+        print('Surviving: ' + str(len(state)))
