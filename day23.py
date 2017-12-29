18cfc46827e252a4079cb21dafdee16b9ea8c1f8
Armin Oonk <Armin@berkelaarmrt.com>
2017-12-29 17:50:45 +0100

    day 23 part 1

---
+++ day23.py
@@ -0,0 +1,53 @@
+with open('day23Input.txt', 'r') as f:
+    data = f.readlines()
+
+cnt = 0
+registers = dict()
+mul_cnt = 0
+
+
+def get_value(v):
+    try:
+        ret = int(v)
+    except:
+        if v in registers:
+            ret = registers[v]
+        else:
+            if v == 'a':
+                ret = 0
+            else:
+                ret = 0
+    return ret
+
+
+def set_value( reg, v):
+    if reg not in registers:
+        registers[reg] = 0
+    registers[reg] = get_value(v)
+
+
+try:
+    while True:
+        vals = data[cnt].strip().split(' ')
+        if vals[0] == 'set':
+            set_value(vals[1], get_value(vals[2]))
+        elif vals[0] == 'sub':
+            set_value(vals[1], get_value(vals[1]) - get_value(vals[2]))
+        elif vals[0] == 'mul':
+            set_value(vals[1], get_value(vals[1]) * get_value(vals[2]))
+            mul_cnt += 1
+        elif vals[0] == 'jnz':
+            if get_value(vals[1]):
+                cnt += get_value(vals[2])
+                continue
+        else:
+            print('Illegal instruction')
+            break
+
+        cnt += 1
+
+except:
+    pass
+
+print("mul called: " + str(mul_cnt) + ' times')
+print("register h is: " + str(registers['h']))
